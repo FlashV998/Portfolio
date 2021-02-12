9df1929f25958eca433dc2296b0263d041d31170
user <flashv998@ubuntu.ubuntu-domain>
2021-02-12 18:31:09 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import React from 'react'
+
+
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar";
+import "./App.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-         hit th eroad baby
-
-      </header>
-    </div>
+    <Navbar/>
   );
 }
 
